08df96a486c1e1c90f1d23cab0a38b52f74dbb8d
Michael Poust <michael.poust221@gmail.com>
2022-06-30 12:44:58 -0400

    exercise 2 solution with tests for factory methods

---
+++ DesignPatterns.Exercises.Tests/CreationalTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using DesignPatterns.Creational;
+
+namespace DesignPatterns.Exercises.Tests
+{
+    [TestFixture]
+    public class CreationalTests
+    {
+        #region Exercise 2 - Factory Tests
+
+        [Test]
+        public void Test()
+        {
+            var pf = new PersonFactory();
+
+            var p1 = pf.CreatePerson("Chris");
+            Assert.That(p1.Name, Is.EqualTo("Chris"));
+            Assert.That(p1.Id, Is.EqualTo(0));
+
+            var p2 = pf.CreatePerson("Sarah");
+            Assert.That(p2.Id, Is.EqualTo(1));
+        }
+
+        #endregion
+    }
+}
---
+++ DesignPatterns/Creational/Exercise2_Factory_Person.cs
@@ -0,0 +1,23 @@
+﻿
+namespace DesignPatterns.Creational
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        private int id = 0;
+
+        public Person CreatePerson(string name)
+        {
+            return new Person { Id = id++, Name = name };
+        }
+    }
+
+    internal class Exercise2_Factory_Person
+    {
+    }
+}
