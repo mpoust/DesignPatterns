3836d955b5229e6af53efbde292a12d58b856ffe
Michael Poust <mpoust@availtec.com>
2022-07-01 09:51:01 -0400

    exercise 5 - adapter square to rectangle

---
+++ DesignPatterns.Exercises.Tests/StructuralTests.cs
@@ -0,0 +1,21 @@
+﻿using DesignPatterns.Structural;
+
+namespace DesignPatterns.Exercises.Tests
+{
+    [TestFixture]
+    public class StructuralTests
+    {
+        #region Exercise 5 - Adapter Tests
+
+        [Test]
+        public void Test()
+        {
+            var sq = new Square { Side = 11 };
+            var adapter = new SquareToRectangleAdapter(sq);
+            Assert.That(adapter.Area(), Is.EqualTo(121));
+        }
+
+        #endregion
+
+    }
+}
--- DesignPatterns/Structural/Exercise5_Adapter_SquareToRectangle.cs
+++ DesignPatterns/Structural/Exercise5_Adapter_SquareToRectangle.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns.Structural
 {
-    internal class Exercise5_Adapter_SquareToRectangle
+    public class Square
     {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        private readonly Square square;
+
+        public SquareToRectangleAdapter(Square square)
+        {
+            this.square = square;
+        }
+
+        public int Width => square.Side;
+        public int Height => square.Side;
     }
 }
